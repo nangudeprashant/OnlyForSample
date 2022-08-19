bae414990d087eb1c1012128e9a612c6f9d82911
abc <abc@xyz.com>
2022-08-19 14:11:32 +0530

    More addition to issue1 and resolved finally.

--- OnlyForSample/src/main/java/com/javalive/Issue1Solution.java
+++ OnlyForSample/src/main/java/com/javalive/Issue1Solution.java
@@ -3,5 +3,6 @@ package com.javalive;
 public class Issue1Solution {
 public static void main(String[] args) {
 	System.out.println("This file is solution for issue 1.");
+	System.out.println("Issue1 resolved finally.");
 }
 }
